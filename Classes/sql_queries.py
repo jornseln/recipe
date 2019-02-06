9631296fd5bd211208151aa0d6d2e5a5a54c873e
Jørn <jornhella@gmail.com>
2019-02-06 17:03:08 +0100

    removed files

--- Classes/sql_queries.py
+++
@@ -1,9 +0,0 @@
-
-class Recipe:
-        def __init__(self, name, image, description, creation_date):
-                self.name = name 
-                self.image = image
-                self.description = description
-                self.creation_date = creation_date
-
-if __name__ == '__main__':
--- Functions/db_con.py
+++
@@ -1,6 +0,0 @@
-
-def ConnectToDatabase():
-    
-
-if __name__ == '__main__':
-    
